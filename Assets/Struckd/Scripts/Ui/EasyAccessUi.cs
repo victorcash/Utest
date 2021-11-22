6fc83e3caecc1290c345922f95ecbd09362b171f
Victor Qian <victor@metagames.gg>
2021-11-22 10:46:55 +0100

    More ui fixes

---
+++ Assets/Struckd/Scripts/Ui/EasyAccessUi.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EasyAccessUi : MonoBehaviour
+{
+    public Button OpenWeather;
+    public Button OpenFile;
+    public Button RemoveAll;
+    public Button test;
+
+    private void Awake()
+    {
+        OpenWeather.onClick.AddListener(OnOpenWeather);
+        OpenFile.onClick.AddListener(OnOpenFile);
+        RemoveAll.onClick.AddListener(ClearMapBtn);
+        test.onClick.AddListener(Test);
+    }
+
+    private void Test()
+    {
+    }
+
+    private void ClearMapBtn()
+    {
+        Services.GameElement.RemoveAllElements();
+    }
+
+    private void OnOpenWeather()
+    {
+        Services.Ui.ToggleWeatherPanel(true);
+    }
+
+    private void OnOpenFile()
+    {
+        Services.Ui.ToggleSaveLoadPanel(true);
+    }
+}
